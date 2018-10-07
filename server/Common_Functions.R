3bef94d488fe837981b77881710788f2ccb3a4a3
dmirnet <38206211+dmirnet@users.noreply.github.com>
2018-10-07 17:15:49 +0900

    Add files via upload

--- server/Common_Functions.R
+++ server/Common_Functions.R
@@ -1,5 +1,8 @@
 ##### DMirNet Common Functions #####
-
+cores<-NULL
+cores_no<-function(core){
+  cores<<-core
+}
 # Matrix normalization based on absolutle values (range: -1 ~ 1)
 norm_mat <- function(mat){
   
@@ -185,10 +188,10 @@ bootstrap <- function(data, direct_fun, ensemble_fun, sample.percentage, iterati
   if (typeof(ensemble_fun) != "character"){
     stop("You must provide the character name of the ensemble method you want to ensemble. For instance, fun=\"IRM\"")
   }
-  rand.seed=seed_num
+  rand_seed=seed_num
   # Bootstrapping function
-  funWrapper <- function(rand.seed, fun, data, sample.percentage,params, ...){
-    set.seed(rand.seed)
+  funWrapper <- function(rand_seed, fun, data, sample.percentage,params, ...){
+    set.seed(rand_seed)
     sampledData <- data[sample(1:nrow(data),round(sample.percentage * nrow(data))),]
     net=NULL
     if(direct_fun=="buildCorpcor"){
@@ -202,7 +205,7 @@ bootstrap <- function(data, direct_fun, ensemble_fun, sample.percentage, iterati
     }
     return(net[upper.tri(net)])
   }
-  cl=makePSOCKcluster(clusters,setup_timeout=120)
+  cl=makePSOCKcluster(cores)
   setDefaultCluster(cl)
   vars=list(direct_fun,"norm_mat","write_file","dir_direct_bootstrap","dir_direct_bootstrap_uppertri","pcor.shrink","space.joint","pc_stable","gaussCItest","getNextSet","udag2pdagRelaxed","idaFast")
   clusterExport(cl, vars, envir = .GlobalEnv)
