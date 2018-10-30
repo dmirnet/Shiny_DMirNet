84992e4053a250d406de17e6a5e9b8e3d450b0d9
dmirnet <38206211+dmirnet@users.noreply.github.com>
2018-10-30 13:55:29 +0900

    Update Common_Functions.R

--- server/Common_Functions.R
+++ server/Common_Functions.R
@@ -210,7 +210,7 @@ bootstrap <- function(data, direct_fun, ensemble_fun, sample.percentage, iterati
   vars=list(direct_fun,"norm_mat","write_file","dir_direct_bootstrap","dir_direct_bootstrap_uppertri","pcor.shrink","space.joint","skeleton_stable","zStat","pcorOrder","invcor.shrink","pc_stable","gaussCItest","getNextSet","udag2pdagRelaxed","idaFast")
   clusterExport(cl, vars, envir = .GlobalEnv)  
   print(.libPaths()[3])  
-  clusterEvalQ(cl, library("corpcor", lib.loc=.libPaths()[3]))
+  clusterEvalQ(cl, library("corpcor", lib.loc=.libPaths()[1]))
   #clusterEvalQ(cl, library("ggplot2"))
   #clusterEvalQ(cl, library("corpcor"))
   #clusterEvalQ(cl, library("space"))  
