23ecffa47689dff2f669d3337106a7996a12f376
dmirnet <38206211+dmirnet@users.noreply.github.com>
2018-10-30 11:39:14 +0900

    Update Common_Functions.R

--- server/Common_Functions.R
+++ server/Common_Functions.R
@@ -207,7 +207,7 @@ bootstrap <- function(data, direct_fun, ensemble_fun, sample.percentage, iterati
   }
   cl=makePSOCKcluster(cores)
   setDefaultCluster(cl)
-  vars=list(direct_fun,"norm_mat","write_file","dir_direct_bootstrap","dir_direct_bootstrap_uppertri","pcor.shrink","space.joint","pc_stable","gaussCItest","getNextSet","udag2pdagRelaxed","idaFast")
+  vars=list(direct_fun,"norm_mat","write_file","dir_direct_bootstrap","dir_direct_bootstrap_uppertri","pcor.shrink","space.joint","skeleton_stable","pc_stable","gaussCItest","getNextSet","udag2pdagRelaxed","idaFast")
   clusterExport(cl, vars, envir = .GlobalEnv)
   result <- parLapply(cl, 1:iterations, funWrapper, fun, data, sample.percentage,params)	
   stopCluster(cl)
